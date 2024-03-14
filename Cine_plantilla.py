da6a28656b77907287530f9068edaf7b306108f3
JoanBes <joanbesalduchlores@gmail.com>
2024-03-14 09:41:41 +0100

    arxiu pkl

--- Cine_plantilla.py
+++ Cine_plantilla.py
@@ -41,38 +41,38 @@ def mostra_menu() -> None:
 #==========================================================================================================
 # Per a simplificar el programa, assumirem que els cines amb les sales estan creats.
         
-# p1 = Pel_licula('La guerra de les galaxies')
-# p2 = Pel_licula('Jocs de guerra')
-# p3 = Pel_licula('Encontres en la 3a fase')
-# p4 = Pel_licula('Indiana Jones')
+# p1 = cg.Pel_licula('La guerra de les galaxies')
+# p2 = cg.Pel_licula('Jocs de guerra')
+# p3 = cg.Pel_licula('Encontres en la 3a fase')
+# p4 = cg.Pel_licula('Indiana Jones')
 
-# pel_licules.append(p1)
-# pel_licules.append(p2)
-# pel_licules.append(p3)
-# pel_licules.append(p4)
+# cg.pel_licules.append(p1)
+# cg.pel_licules.append(p2)
+# cg.pel_licules.append(p3)
+# cg.pel_licules.append(p4)
 
-# c1 = Cine('La salera')
-# c2 = Cine('Estepark')
-# cines.append(c1)
-# cines.append(c2)
+# c1 = cg.Cine('La salera')
+# c2 = cg.Cine('Estepark')
+# cg.cines.append(c1)
+# cg.cines.append(c2)
 
-# sala1_1 = Sala(c1, 'sala 1', 4, 4)
-# sala2_1 = Sala(c1, 'sala 2', 5, 5)
-# sala1_2 = Sala(c2, 'sala 1', 4, 4)
-# sala2_2 = Sala(c2, 'sala 2', 5, 5)
+# sala1_1 = cg.Sala(c1, 'sala 1', 4, 4)
+# sala2_1 = cg.Sala(c1, 'sala 2', 5, 5)
+# sala1_2 = cg.Sala(c2, 'sala 1', 4, 4)
+# sala2_2 = cg.Sala(c2, 'sala 2', 5, 5)
 
 # data1= dt.datetime(2024, 1, 1, 16, 0, 0)
 # data2= dt.datetime(2024, 1, 1, 20, 0, 0)
 
-# Sessio(sala1_1,data1,p1,5)
-# Sessio(sala1_1,data2,p1,6)
-# Sessio(sala2_1,data1,p2,5)
-# Sessio(sala2_1,data2,p2,6)
+# cg.Sessio(sala1_1,data1,p1,5)
+# cg.Sessio(sala1_1,data2,p1,6)
+# cg.Sessio(sala2_1,data1,p2,5)
+# cg.Sessio(sala2_1,data2,p2,6)
 
-# Sessio(sala1_2,data1,p1,5)
-# Sessio(sala1_2,data2,p2,6)
-# Sessio(sala2_2,data1,p3,5)
-# Sessio(sala2_2,data2,p3,6)
+# cg.Sessio(sala1_2,data1,p1,5)
+# cg.Sessio(sala1_2,data2,p2,6)
+# cg.Sessio(sala2_2,data1,p3,5)
+# cg.Sessio(sala2_2,data2,p3,6)
 
 if __name__ == "__main__":
     cg.llig_arxiu()
